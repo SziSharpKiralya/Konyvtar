fd5eb9d40038338fec8abc26e724d85b0269053a
Csengeri Marcell <csengeri.marcell@petrik.hu>
2025-03-11 16:18:55 +0100

    Getterek és konstruktor kész

--- Konyvtar/Book.cs
+++ Konyvtar/Book.cs
@@ -8,6 +8,23 @@ namespace Konyvtar
 {
     internal class Book
     {
+        private string title;
+        private string author;
+        private int pageCount;
+        private int releaseYear;
+
+        public string Title { get => title; set => title = value; }
+        public string Author { get => author; set => author = value; }
+        public int PageCount { get => pageCount; set => pageCount = value; }
+        public int ReleaseYear { get => releaseYear; set => releaseYear = value; }
+
+        public Book(string title, string author, int pageCount, int releaseYear)
+        {
+            this.title = title;
+            this.author = author;
+            this.pageCount = pageCount;
+            this.releaseYear = releaseYear;
+        }
 
     }
 }
